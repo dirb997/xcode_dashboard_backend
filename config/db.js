2467acaefcf1972380a39e4f8d433babbdccef7c
Diego Berlanga <jdruizberlanga@gmail.com>
2025-03-07 17:00:20 +0900

    Updated to mysql2 to be able to use autehntication plugin caching_sha2_password
    Installed new dependencies:
    - mysql2

--- config/db.js
+++ config/db.js
@@ -1,16 +1,16 @@
 // File that configure the connection to the database
 
-const mysql = require('mysql');
+const mysql = require('mysql2');
 const db = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'password',
+    password: '',
     database: 'xcode_user_db'
 });
 
 db.connect((err) => {
     if (err) {
-        console.log('Error connecting to database: ' + err);
+        console.log('Error connecting to database:\n' + err);
     } else {
         console.log('Connected to database');
     }
