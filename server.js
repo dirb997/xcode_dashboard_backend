729067d47c3e1b8f3aa0e93ef55f4fad73d34554
Diego Berlanga <jdruizberlanga@gmail.com>
2025-03-10 17:27:11 +0900

    Added CORS settings

--- server.js
+++ server.js
@@ -6,6 +6,11 @@ const app = express();
 app.use(bodyParser.json());
 
 app.use('/api', userRoutes);
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Content-Type");
+    next();
+});
 
 const PORT = 3000;
 app.listen(PORT, () => {
